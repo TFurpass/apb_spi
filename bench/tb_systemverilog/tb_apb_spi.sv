eac644502cb1aacf7513609cf28ed66eb7c15023
furpass <toni.furpass@tuni.fi>
2026-07-20 15:14:00 +0300

    tb gets spi data out
    spi data seems to be coming from spiaddr, spicmd and fifo do not seem to be sent.

--- bench/tb_systemverilog/tb_apb_spi.sv
+++ bench/tb_systemverilog/tb_apb_spi.sv
@@ -16,20 +16,68 @@ module tb_apb_spi #() ();
 
     // Unused out & in of DUT
     logic   unused_out, unused_in;
+    (* keep *)
+    logic spi_clk, mosi, miso, csn;
+    (* keep *)
+    logic o1,o2,o3 /* verilator public */;
+    (* keep *)
+    logic i0, i1, i2, i3 /* verilator public */;
+    // TODO correct bit widths
+    logic [8:0] DUT_STATUS_REG; // shows STATUS_REG signals
+    logic [7:0] REG_CLKDIV; 
+    logic [31:0] REG_SPICMD; 
+    logic [31:0] REG_SPIADR; 
+    logic [27:0] REG_SPILEN; 
+    logic [8:0] REG_SPIDUM; 
+    logic [8:0] REG_TXFIFO; 
+    logic [8:0] REG_RXFIFO; 
+    logic [8:0] REG_INTCFG; 
+    logic [8:0] REG_INTSTA; 
+    logic [3:0] write_address;
+
+    assign write_address = i_dut.u_axiregs.write_address;
     assign  unused_in = 1'b0;
 
+    assign REG_CLKDIV = {
+        i_dut.u_axiregs.spi_clk_div
+    };
+
+    assign REG_SPILEN = {
+        i_dut.u_axiregs.spi_data_len,
+        i_dut.u_axiregs.spi_addr_len,
+        i_dut.u_axiregs.spi_cmd_len
+    };
+
+    assign REG_SPICMD = {
+        i_dut.u_axiregs.spi_cmd
+    };
+
+    assign REG_SPIADR = {
+        i_dut.u_axiregs.spi_addr
+    };
+    // TODO Other register assignments
+    assign DUT_STATUS_REG = {
+        i_dut.u_axiregs.spi_csreg,
+        i_dut.u_axiregs.spi_swrst,
+        i_dut.u_axiregs.spi_qwr,
+        i_dut.u_axiregs.spi_qrd,
+        i_dut.u_axiregs.spi_wr,
+        i_dut.u_axiregs.spi_rd
+        };
 
     initial begin
         $dumpfile("build/verilator_build/wave.vcd");
         $dumpvars(0, i_dut);
         $display("\n\t###TESTING###");
         #500ns;
-        i_vip.test_APB_REG_write_read(12'b0000, 32'b1000, 1);
-        $finish();
+        //i_vip.test_APB_REG_write_read(12'b0000, 32'b1000, 1);
+        $display("\n\t###TESTING CMD0###");
+        i_vip.CMD(0);
+        
     end
 
     apb_spi_master #() i_dut (
-        .HCLK (apb_bus.PCLK),
+        .HCLK (apb_bus.APB_Slave.PCLK),
         .HRESETn (apb_bus.PRESETn),
         .PADDR (apb_bus.PADDR),
         .PWDATA (apb_bus.PWDATA),
@@ -42,22 +90,23 @@ module tb_apb_spi #() ();
 
         .events_o(),
 
-        .spi_clk(), // connect to SPI SD-Card
-        .spi_csn0(), // connect to SPI SD-Card
+        .spi_clk(spi_clk), // connect to SPI SD-Card
+        .spi_csn0(csn), // connect to SPI SD-Card
         .spi_csn1(),
         .spi_csn2(),
         .spi_csn3(),
         .spi_mode(),
-        .spi_sdo0(), // connect to SPI SD-Card
-        .spi_sdo1(),
-        .spi_sdo2(),
-        .spi_sdo3(),
-        .spi_sdi0(unused_in), 
-        .spi_sdi1(), // connect to SPI SD-Card
-        .spi_sdi2(unused_in),
-        .spi_sdi3(unused_in)
+        .spi_sdo0(mosi), // connect to SPI SD-Card
+        .spi_sdo1(o1),
+        .spi_sdo2(o2),
+        .spi_sdo3(o3),
+        .spi_sdi0(i0), 
+        .spi_sdi1(miso), // connect to SPI SD-Card
+        .spi_sdi2(i2),
+        .spi_sdi3(i3)
     );
     
+
     vip_apb_spi #() i_vip (
         .apb_mst (apb_bus.APB_Master)
         /* .spi_mst (unu),
--- bench/tb_systemverilog/vip_apb_spi_top.sv
+++ bench/tb_systemverilog/vip_apb_spi_top.sv
@@ -1,3 +1,14 @@
+`define STATUS_ADDR  8'h0 // BASEREG + 0x00
+`define CLKDIV_ADDR  8'h0004 // BASEREG + 0x04
+`define SPICMD_ADDR  8'h08 // BASEREG + 0x08
+`define SPIADDR_ADDR 8'h0C // BASEREG + 0x0C
+`define SPILEN_ADDR  8'h10 // BASEREG + 0x10
+`define SPIDUM_ADDR  8'h14 // BASEREG + 0x14
+`define TXFIFO_ADDR  8'h18 // BASEREG + 0x18
+`define RXFIFO_ADDR  8'h20 // BASEREG + 0x20
+`define INTCFG_ADDR  8'h24 // BASEREG + 0x24
+`define INTSTA_ADDR  8'h28 // BASEREG + 0x28
+
 module vip_apb_spi #() (
     apb_interface.APB_Master apb_mst
     /* spi_interface.SPI_Master spi_mst,
@@ -15,12 +26,12 @@ module vip_apb_spi #() (
 // );
 
     localparam time clk_cycle = 10ns;
-    localparam longint unsigned SimCycles = 'd1_000;
+    localparam longint unsigned SimCycles = 'd5_000;
     logic clk, rst_n;
 
     clk_rst_gen # (
         .ClkPeriod (clk_cycle),
-        .RstClkCycles (15)
+        .RstClkCycles (5)
     ) i_clk_rst (
         .clk_o (clk),
         .rst_no (apb_mst.PRESETn)
@@ -39,19 +50,128 @@ module vip_apb_spi #() (
         .apb_mst(apb_mst)
     );
 
+    // TODO display all register info, since some are write only.
     task automatic test_APB_REG_write_read (logic [11:0] addr, logic [31:0] write_data, bit manual_data);
+        
         automatic logic [31:0] read_data = 0;
         automatic logic [31:0] data = 0;
+
         if(manual_data == 1) begin
             data = write_data;
         end else begin
             data = $urandom();
         end
+
         $display("[VIP] Write-read test with APB interface on address 0x%8H", addr);
         i_apb.write(addr, data);
         i_apb.read(addr, read_data);
         $display("[VIP] Write-data: 0x%8H", data);
         $display("[VIP] read-data: 0x%8H", read_data);
+
+    endtask
+
+    task automatic CMD (logic [7:0] CMD);
+        
+        automatic logic [31:0] read_data = 0;
+        automatic logic [31:0] data = 0;
+        logic [31:0] CMD0_data = 32'h40;
+        logic [11:0] addr;
+
+        $display("[VIP] Writing to STATUS_REG (addr: 0x%8H) the value: 0x%8H", addr, data);
+        
+        //TODO vector for address order depending on command
+        case(CMD)
+            0: begin
+                addr = 12'(`STATUS_ADDR);
+                data = 32'h0008;
+                $display("[VIP] Writing to (addr: 0x%8H) the value: 0x%8H", addr, data);
+                i_apb.write(addr, data);
+                #50ns
+                addr = 12'(`SPILEN_ADDR);
+                data = 32'h10002808;
+                $display("[VIP] Writing to (addr: 0x%8H) the value: 0x%8H", addr, data);
+                i_apb.write(addr, data );
+                addr = 12'(`CLKDIV_ADDR);
+                data = 32'h00;
+                $display("[VIP] Writing to (addr: 0x%8H) the value: 0x%8H", addr, data);
+                i_apb.write(addr, data);
+                  addr = 12'(`STATUS_ADDR);
+                data = 32'h0102;
+                $display("[VIP] Writing to (addr: 0x%8H) the value: 0x%8H", addr, data);
+                i_apb.write(addr, data);
+                addr = 12'(`SPICMD_ADDR);
+                data = 32'h40;
+                $display("[VIP] Writing to (addr: 0x%8H) the value: 0x%8H", addr, data);
+                i_apb.write(addr, data);
+                addr = 12'(`SPIADDR_ADDR);
+                data = 32'h95;
+                $display("[VIP] Writing to (addr: 0x%8H) the value: 0x%8H", addr, data);
+                i_apb.write(addr,data);
+                addr = 12'(`TXFIFO_ADDR);
+                data = 32'h0102;
+                i_apb.write(addr, data);
+                 addr = 12'(`STATUS_ADDR);
+                data = 32'h0102;
+                $display("[VIP] Writing to (addr: 0x%8H) the value: 0x%8H", addr, data);
+                
+
+            end
+            default: begin
+                addr = 12'(`SPILEN_ADDR);
+                data = 32'h10002808;
+                i_apb.write(addr, data);
+                addr = 12'(`CLKDIV_ADDR);
+                data = 32'hFA;
+                i_apb.write(addr, data);
+                addr = 12'(`SPICMD_ADDR);
+                data = 32'h40;
+                i_apb.write(addr, data);
+                addr = 12'( `SPIADDR_ADDR);
+                data = 32'h95;
+                i_apb.write(addr,data);
+                addr = 12'(`STATUS_ADDR);
+                data = 32'h0102;
+                i_apb.write(addr, data);
+            end
+        endcase
+/* 
+        case(addr)
+
+            0: begin
+                $display("REG_STATUS");
+                data = 32'h0102;
+                $display("Writing 0x%8H", data);
+            end
+
+            4: begin 
+                logic div_value;
+                $display("CLKDIV_ADDR");
+            end
+            
+            8: begin
+                $display("SPICMD_ADDR");
+            end
+
+            12: $display("SPIADDR_ADDR");
+
+            16: begin
+                data = 32'h10002808;
+                $display("SPILEN_ADDR");
+                $display("Setting SPICMD length to 8 bits");
+                $display("SPIADDR to 40 bits for additional fields and CRC");
+                $display("FIFO to 512KB");
+                $display("Writing 0x%8H", data);
+            end
+
+            20: $display("SPIDUM_ADDR");
+            24: $display("TXFIFO_ADDR");
+            28: $display("RXFIFO_ADDR");
+            32: $display("INTCFG_ADDR");
+            36: $display("INTSTA_ADDR");
+            default: $display("No register mapped to given addr");
+        endcase */
+        //TODO add addr check.
+
 
     endtask
 
